e72d6ab5445870134eab195df7fe95470abf274b
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2020-03-19 21:26:21 -0300

    structure settings

--- src/app.js
+++ src/app.js
@@ -2,19 +2,18 @@ import express from "express"
 import routes from "./routes"
 
 class App {
-    constructor(){
-        this.server = express()
-        this.middlewares()
-        this.routes()
-    }
-    
-    middlewares(){
-        this.server.use(express.json())
-    }
+  constructor() {
+    this.server = express()
+    this.middlewares()
+    this.routes()
+  }
+  middlewares() {
+    this.server.use(express.json())
+  }
 
-    routes(){
-        this.server.use(routes)
-    }
+  routes() {
+    this.server.use(routes)
+  }
 }
 
 export default new App().server
