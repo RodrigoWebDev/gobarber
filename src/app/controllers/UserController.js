dc171bb0e13c0d531be0380532f9776037c13f95
rodrigowebdev <rodrigo.queiroz.chagas@gmail.com>
2020-04-01 13:54:44 -0300

    user update

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -3,6 +3,8 @@ import User from "../models/User"
 class UserController {
   async store(req, res) {
     const userExists = await User.findOne({ where: { email: req.body.email } })
+
+    //Check if the email that the user want to create already exsit
     if (userExists) {
       return res.status(400).json({ erro: "User already exist" })
     }
@@ -12,8 +14,28 @@ class UserController {
   }
 
   async update(req, res) {
-    console.log(req.userId)
-    return res.json({ ok: true })
+    const { email, oldPassword } = req.body
+
+    const user = await User.findByPk(req.userId)
+
+    // check if the email that the user want to change to already exist
+    if (email && email != user.email) {
+      const userExists = await User.findOne({ where: { email } })
+      if (userExists) {
+        return res.status(400).json({ error: "User already exist" })
+      }
+    }
+
+    //Chekc if the old password matches
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return res.status(401).json({ error: "Password does not match" })
+    }
+
+    const { id, name, provider } = await user.update(req.body)
+
+    console.log(id, name, email, provider)
+
+    return res.json({ id, name, email, provider })
   }
 }
 
